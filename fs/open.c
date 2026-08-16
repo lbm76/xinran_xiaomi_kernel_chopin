63db4ab78a1f4a7ed277c3082b1205ffe5c96f61
lbm76 <3663907882@qq.com>
2026-08-17 02:27:45 +0800

    Add files via upload

--- fs/open.c
+++ fs/open.c
@@ -32,6 +32,10 @@
 #include <linux/dnotify.h>
 #include <linux/compat.h>
 
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs.h>
+#endif
+
 #include "internal.h"
 
 int do_truncate2(struct vfsmount *mnt, struct dentry *dentry, loff_t length,
